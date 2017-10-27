70c5fa061fa9c84da5d5725cceb1eda4d44e8a54
uaj14 <an.jon@mail.utoronto.ca>
2017-10-27 09:35:38 -0400

    Added double function

--- py.py
+++ py.py
@@ -1,3 +1,6 @@
+def db(x):
+    return x * 2
+
 """Returns (x+1)^(x+1)""" # Line 1-2 changed in u1
 
 def ex(x):
@@ -6,3 +9,4 @@ def ex(x):
     return x**x
 # This line changed in u0 # and again in u1
 print(ex(1))
+print(db(10))
